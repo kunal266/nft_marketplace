cff3b81d3d3b114d14c24ff355a32b94e529ff89
Kunal Makwana <57712165+kunal266@users.noreply.github.com>
2022-01-09 11:59:30 +0530

    Update Mintnft.js

--- src/Components/Mintnft.js
+++ src/Components/Mintnft.js
@@ -1,7 +1,7 @@
 import React, { useState,useRef } from "react";
 import { NFTStorage, File } from 'nft.storage/dist/bundle.esm.min.js';
 import fs from "fs";
-const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGY2YTlBNDgxZDM1ZWZhMEJmNTUwRkVDY2I5YjJjMzYyRDREQzIwNzQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MDUxNjM2OTU3OSwibmFtZSI6InN1cGFzdGF6eiJ9.YqCV_iWK8HPupN6h1tILk14TLfMqG51GY_gCDDz5R5Y'
+const apiKey = "<YOUR_API_KEY>"
 const client = new NFTStorage({ token: apiKey })
 import * as nearAPI from 'near-api-js';
 const Mintnft = () => {
